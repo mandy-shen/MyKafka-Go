b03cb4bd6364dcf15b0ce65526001e1dee85eac8
mandy-shen <shen.mo@northeastern.edu>
2021-04-14 06:44:13 -0700

    better statement

--- kafka/kfk_start.sh
+++ kafka/kfk_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export KAFKA_HOME=/kafka_2.12-2.7.0
+export KAFKA_HOME=/kafka_$KAFKA_VER
 $KAFKA_HOME/bin/zookeeper-server-start.sh -daemon $KAFKA_HOME/config/zookeeper.properties \
-&& until echo ruok | nc 127.0.0.1 2181 > /dev/null; do sleep 1; done \
-&& $KAFKA_HOME/bin/kafka-server-start.sh -daemon $KAFKA_HOME/config/server.properties \
-&& until echo ruok | nc 127.0.0.1 9092 > /dev/null; do sleep 1; done
+  && until echo ruok | nc 127.0.0.1 2181 > /dev/null; do sleep 1; done \
+  && $KAFKA_HOME/bin/kafka-server-start.sh -daemon $KAFKA_HOME/config/server.properties \
+  && until echo ruok | nc 127.0.0.1 9092 > /dev/null; do sleep 1; done
